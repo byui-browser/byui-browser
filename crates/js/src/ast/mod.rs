fa5ec884737702f78bddeb62c906c2ccdcac023e
nsauce <58062592+Daboigenius342@users.noreply.github.com>
2026-09-24 21:52:45 -0600

    Resolve AST merge conflicts and fix Rust syntax

--- crates/js/src/ast/mod.rs
+++ crates/js/src/ast/mod.rs
@@ -1,16 +1,13 @@
-<<<<<<< Updated upstream
-/// Expressions produced by the parser.
-#[derive(Debug, PartialEq)]
-=======
+/// A parsed JavaScript program.
 #[derive(Debug, Clone, PartialEq)]
 pub struct Program {
-    pub body: Vec<statement>,
+    pub body: Vec<Statement>,
 }
->>>>>>> Stashed changes
+
+/// Expressions produced by the parser.
+#[derive(Debug, Clone, PartialEq)]
 pub enum Expr {
     Number(f64),
-<<<<<<< Updated upstream
-=======
     String(String),
     Boolean(bool),
     Null,
@@ -20,34 +17,26 @@ pub enum Expr {
         operator: UnaryOperator,
         operand: Box<Expr>,
     },
->>>>>>> Stashed changes
     Binary {
         left: Box<Expr>,
         operator: BinaryOperator,
         right: Box<Expr>,
     },
-    Logical{
-        left:Box<Expr>
+    Logical {
+        left: Box<Expr>,
         operator: LogicalOperator,
-        right: Box<Expr>
+        right: Box<Expr>,
     },
     Assign {
-        name: string,
-        value: Box<Expr>
+        name: String,
+        value: Box<Expr>,
     },
     Call {
         callee: Box<Expr>,
-        arguments: Vec<Expr>
+        arguments: Vec<Expr>,
     },
-
-
-
-
 }
 
-<<<<<<< Updated upstream
-/// Operators that combine two expressions.
-=======
 #[derive(Debug, Clone, PartialEq)]
 pub enum Statement {
     Block(Vec<Statement>),
@@ -66,8 +55,7 @@ pub enum Statement {
         condition: Expr,
         body: Box<Statement>,
     },
-
-    FunctionDeclaration{
+    FunctionDeclaration {
         name: String,
         params: Vec<String>,
         body: Vec<Statement>,
@@ -90,7 +78,7 @@ pub enum UnaryOperator {
     Not,
 }
 
->>>>>>> Stashed changes
+/// Operators that combine two expressions.
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum BinaryOperator {
     Add,
@@ -106,7 +94,6 @@ pub enum BinaryOperator {
     NotEqual,
     StrictEqual,
     StrictNotEqual,
-
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -114,65 +101,63 @@ pub enum LogicalOperator {
     And,
     Or,
 }
-#cfg[(test)]
+
+#[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
-    fn let_with_arithmetic(){
-        let statement = Statement::VariableDeclaration{
-            kind: Varkind::Let,
+    fn let_with_arithmetic() {
+        let statement = Statement::VariableDeclaration {
+            kind: VarKind::Let,
             name: "count".to_string(),
             init: Some(Expr::Binary {
                 left: Box::new(Expr::Number(1.0)),
                 operator: BinaryOperator::Add,
                 right: Box::new(Expr::Number(2.0)),
             }),
-            };
+        };
         assert_eq!(statement.clone(), statement);
     }
+
     #[test]
-    fn if_with_block(){
+    fn if_with_block() {
         let statement = Statement::If {
-            condition: Expr::Binary{
+            condition: Expr::Binary {
                 left: Box::new(Expr::Identifier("x".to_string())),
                 operator: BinaryOperator::Greater,
                 right: Box::new(Expr::Number(5.0)),
             },
-            then_branch: Box::new(Statement::Block(vec!
-                {Statement::Expression(
-                    Expr::Assign {
+            then_branch: Box::new(Statement::Block(vec![Statement::Expression(
+                Expr::Assign {
                     name: "x".to_string(),
                     value: Box::new(Expr::Number(0.0)),
-                    },
-                )})),
+                },
+            )])),
             else_branch: None,
         };
         assert_eq!(statement.clone(), statement);
     }
+
     #[test]
-    fn function_and_call(){
-        let program = Program{
+    fn function_and_call() {
+        let program = Program {
             body: vec![
-                Statement::FunctionDeclaration{
+                Statement::FunctionDeclaration {
                     name: "add".to_string(),
-                    params: vec!["a".to_string(),
-                        "b".to_string()],
-                    body: vec!
-    [Statement:: Return(Some(Expr::Binary{
-                    left: Box::new(Expr::Identifier("a".to_string())),
-                    operator: BinaryOperator::Add,
-                    right: Box::new(Expr::Identifier("b".to_string())),
-    }))],
+                    params: vec!["a".to_string(), "b".to_string()],
+                    body: vec![Statement::Return(Some(Expr::Binary {
+                        left: Box::new(Expr::Identifier("a".to_string())),
+                        operator: BinaryOperator::Add,
+                        right: Box::new(Expr::Identifier("b".to_string())),
+                    }))],
                 },
                 Statement::Expression(Expr::Call {
-                    callee:
-                    Box::new(Expr::Identifier("Add".to_string())),
-                    arguments: vec![Expr::Number(1.0),
-                        Expr::Number(2.0)],
-
+                    callee: Box::new(Expr::Identifier("Add".to_string())),
+                    arguments: vec![Expr::Number(1.0), Expr::Number(2.0)],
                 }),
             ],
         };
-        assert_eq!(program.body.len(),2);
+        assert_eq!(program.body.len(), 2);
     }
 }
